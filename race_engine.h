89510174ffbbacfff1c9fff57191a6ac955e6ea1
Brandon Wu <brandonwu23@gmail.com>
2023-03-27 16:20:52 -0700

    Define interface between race and strategy engines

---
+++ race_engine.h
@@ -0,0 +1,14 @@
+// Copyright 2023 Aaramse Project
+
+#pragma once
+
+#include <list>
+
+#include "node.h"
+
+class RaceEngine
+{
+    public:
+    std::list<Node> GetGridOrder() const;
+    void tick const();
+};
